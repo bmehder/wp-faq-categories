b32289eb95bd602cab9e62d0a85a0b05d35631b3
bmehder <bmehder@gmail.com>
2022-05-15 01:41:18 +0200

    v2 - FP style, added Items

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,53 @@
 <script>
-  import { scale } from 'svelte/transition'
-
   import Loading from './Loading.svelte'
-  import Faq from './Faq.svelte'
+  import Items from './Items.svelte'
 
-  const URL = 'https://sprucehealthgroup.com/wp-json/wp/v2/'
+  const BASE_URL = 'https://sprucehealthgroup.com/wp-json/wp/v2/'
   const FAQ_CATS = 'faq-cats'
   const FAQS_BY_CAT = 'faqs?faq-cats'
 
   let items = []
   let isLoaded = false
 
+  const responseCb = res => res.json()
+  const errorCb = error => console.error(error)
+
   const getFaqsByCategory = (category, numberOfCategories) => {
+    const endpoint = `${BASE_URL}${FAQS_BY_CAT}=${category.id}`
+
     const isFinished = () => items.length === numberOfCategories
-    const setIsLoaded = () => (isLoaded = true)
 
     const compareSortOrderByProp = prop => (a, b) => a[prop] > b[prop] ? 1 : -1
+
     const sortByProp = (array, prop) => array.sort(compareSortOrderByProp(prop))
 
     const setItems = ({ slug, name }, faqs) => (items = [...items, { slug, name, faqs }])
 
-    fetch(`${URL}${FAQS_BY_CAT}=${category.id}`)
-      .then(res => res.json())
-      .then(faqs => setItems(category, faqs))
-      .catch(error => console.error(error))
-      .finally(() => isFinished() && sortByProp(items, 'slug') && setIsLoaded())
+    const setIsLoaded = () => (isLoaded = true)
+
+    const dataCb = faqs => setItems(category, faqs)
+
+    const finallyCb = () => isFinished() && sortByProp(items, 'slug') && setIsLoaded()
+
+    fetch(endpoint).then(responseCb).then(dataCb).catch(errorCb).finally(finallyCb)
   }
 
-  const getCategories = () => {
-    fetch(URL + FAQ_CATS)
-      .then(res => res.json())
-      .then(categories =>
-        categories.map(category => getFaqsByCategory(category, categories.length))
-      )
-      .catch(error => console.error(error))
+  const getFaqCategories = () => {
+    const endpoint = BASE_URL + FAQ_CATS
+
+    const loop = data => category => getFaqsByCategory(category, data.length)
+
+    const dataCb = data => data.map(loop(data))
+
+    fetch(endpoint).then(responseCb).then(dataCb).catch(errorCb)
   }
 
-  const init = () => getCategories()
+  const init = () => getFaqCategories()
 </script>
 
 <div use:init>
   {#if isLoaded}
-    <div in:scale>
-      {#each items as item}
-        {@const { name, faqs } = item}
-        <h2>{name}</h2>
-        {#each faqs as faq}
-          <Faq {faq} />
-        {/each}
-      {/each}
-    </div>
+    <Items {items} />
   {:else}
     <Loading />
   {/if}
---
+++ src/Items.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { scale } from 'svelte/transition'
+  import Faq from './Faq.svelte'
+
+  export let items
+</script>
+
+<div in:scale>
+  {#each items as { name, faqs }}
+    <h2>{name}</h2>
+    {#each faqs as faq}
+      <Faq {faq} />
+    {/each}
+  {/each}
+</div>
